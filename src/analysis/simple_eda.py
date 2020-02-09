2a14d7d53865bbadf8e584a267f0f61a83682b7c
Spencer Bertsch <spencerbertsch99@gmail.com>
2020-02-09 14:01:15 -0500

    begin simple eda on structured data

---
+++ src/analysis/simple_eda.py
@@ -0,0 +1,10 @@
+"""
+
+Simply explore the data from the kaggle competition
+
+Use hostograms and correlation matrices to look at the relationship between the features and the label
+
+"""
+
+# TODO import combined data (after the combine_data.py script runs)
+
