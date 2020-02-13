84130f7dacfd8ec4bbe3bad87fa23c587d0d77d6
Spencer Bertsch <spencerbertsch99@gmail.com>
2020-02-12 21:53:07 -0500

    Add main method and other updates

--- src/data_processing/combine_data.py
+++ src/data_processing/combine_data.py
@@ -2,9 +2,15 @@
 
 Combine the files from the kaggle competition into a single dataframe
 
+This file can be run as a python script by calling the main method at the bottom of the file.
+
 """
 
-# Imports
+# native imports
+import argparse
+import path
+
+# package imports
 import pandas as pd
 import numpy as np
 import os
@@ -13,7 +19,24 @@ import matplotlib.pyplot as plt
 """
 Import raw data from .csv files for cleaning and combination
 """
-transactions    = pd.read_csv(os.path.join(DATA_FOLDER, 'sales_train.csv.gz'))
-items           = pd.read_csv(os.path.join(DATA_FOLDER, 'items.csv'))
-item_categories = pd.read_csv(os.path.join(DATA_FOLDER, 'item_categories.csv'))
-shops           = pd.read_csv(os.path.join(DATA_FOLDER, 'shops.csv'))
+
+def import_data(path: str):
+    path = '/Users/spencerbertsch/Desktop/Courses/Coursera/Predict-Future-Sales-Kaggle/src/data/'
+    transactions    = pd.read_csv(str(path + 'sales_train.csv'))
+    items           = pd.read_csv(str(path + 'items.csv'))
+    item_categories = pd.read_csv(str(path + 'item_categories.csv'))
+    shops           = pd.read_csv(str(path + 'shops.csv'))
+
+
+if __name__ == '__main__':
+    print('...combining csv files...')
+    parser = argparse.ArgumentParser(description='Import csv files for merging')
+    parser.add_argument('--import_path', type=str,
+                        help='path to the directory containing all csv files for import')
+
+    args = parser.parse_args()
+    import_path = args.import_path
+    print(import_path)
+
+    # import data
+    import_data(path=import_path)
